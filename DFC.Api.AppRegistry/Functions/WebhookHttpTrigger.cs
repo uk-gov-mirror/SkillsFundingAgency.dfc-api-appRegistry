b54f74dd39ece810a2cbd9348f1a9479b3b99320
Tom Tollafield <thomas.tollafield@gmail.com>
2020-07-16 13:36:22 +0100

    WIP

---
+++ DFC.Api.AppRegistry/Functions/WebhookHttpTrigger.cs
@@ -0,0 +1,86 @@
+﻿using DFC.Swagger.Standard.Annotations;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace DFC.Api.AppRegistry.Functions
+{
+    public class WebhookHttpTrigger
+    {
+        private readonly ILogger<WebhookHttpTrigger> logger;
+        private readonly IWebhookReceiver webhookReceiver;
+
+        public WebhookHttpTrigger(
+           ILogger<WebhookHttpTrigger> logger,
+           IDocumentService<AppRegistrationModel> documentService)
+        {
+            this.logger = logger;
+            this.documentService = documentService;
+        }
+
+        [FunctionName("Webhook")]
+        [Display(Name = "Post an app registration", Description = "Creates a new resource of type 'AppRegistry'.")]
+        [ProducesResponseType(typeof(AppRegistrationModel), (int)HttpStatusCode.Created)]
+        [Response(HttpStatusCode = (int)HttpStatusCode.Created, Description = "App Registration created", ShowSchema = true)]
+        [Response(HttpStatusCode = (int)HttpStatusCode.OK, Description = "App Registration updated", ShowSchema = false)]
+        [Response(HttpStatusCode = (int)HttpStatusCode.Unauthorized, Description = "API key is unknown or invalid", ShowSchema = false)]
+        [Response(HttpStatusCode = (int)HttpStatusCode.Forbidden, Description = "Insufficient access", ShowSchema = false)]
+        [Response(HttpStatusCode = (int)HttpStatusCode.BadRequest, Description = "Request was malformed", ShowSchema = false)]
+        [Response(HttpStatusCode = (int)HttpStatusCode.UnprocessableEntity, Description = "AppRegistry validation error(s)", ShowSchema = false)]
+        [Response(HttpStatusCode = (int)HttpStatusCode.TooManyRequests, Description = "Too many requests being sent, by default the API supports 150 per minute.", ShowSchema = false)]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "webhook/")] HttpRequest? request)
+        {
+            logger.LogInformation("Validating Post AppRegistration for");
+
+            if (request == null)
+            {
+                logger.LogWarning($"Missing request.Body");
+                return new BadRequestResult();
+            }
+
+            var appRegistrationModel = await request.GetModelFromBodyAsync<AppRegistrationModel>(logger).ConfigureAwait(false);
+
+            if (appRegistrationModel == null)
+            {
+                logger.LogWarning($"Request.Body is malformed");
+                return new BadRequestResult();
+            }
+
+            try
+            {
+                logger.LogInformation($"Attempting to create app registration for: {appRegistrationModel.Path}");
+
+                appRegistrationModel.Regions?.ForEach(f => f.DateOfRegistration = DateTime.UtcNow);
+                appRegistrationModel.DateOfRegistration = DateTime.UtcNow;
+
+                var statusCode = await documentService.UpsertAsync(appRegistrationModel).ConfigureAwait(false);
+
+                switch (statusCode)
+                {
+                    case HttpStatusCode.Created:
+                        logger.LogInformation($"Created app registration with Post for: {appRegistrationModel.Path}: Status code {statusCode}");
+                        var resultModel = await documentService.GetAsync(p => p.Path == appRegistrationModel.Path).ConfigureAwait(false);
+                        return new CreatedResult(resultModel!.Path, resultModel);
+                    case HttpStatusCode.OK:
+                        logger.LogInformation($"Upserted app registration with Post for: {appRegistrationModel.Path}: Status code {statusCode}");
+                        return new OkResult();
+                }
+
+                logger.LogError($"Error creating app registration with Post for: {appRegistrationModel.Path}: Status code {statusCode}");
+                return new UnprocessableEntityResult();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, $"Error creating app registration with Post for: {appRegistrationModel.Path}");
+                return new UnprocessableEntityResult();
+            }
+        }
+    }
+}
