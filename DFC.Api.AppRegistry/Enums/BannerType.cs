e187d6aec1f91a7de90055874e5552bc82bfef55
Tom Tollafield <thomas.tollafield@gmail.com>
2020-08-10 15:03:06 +0100

    Banner WIP

---
+++ DFC.Api.AppRegistry/Enums/BannerType.cs
@@ -0,0 +1,9 @@
+﻿namespace DFC.Api.AppRegistry.Enums
+{
+    public enum BannerType
+    {
+        NotableDeath = 0,
+        NationalEmergency = 1,
+        LocalEmergency = 2,
+    }
+}
---
+++ DFC.Api.AppRegistry/Models/BannerModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DFC.Api.AppRegistry.Models
+{
+    public class BannerModel
+    {
+        public string? Heading { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public Uri URL { get; set; }
+
+        public string LinkText { get; set; }
+
+        public bool GlobalBanner { get; set; }
+    }
+}
